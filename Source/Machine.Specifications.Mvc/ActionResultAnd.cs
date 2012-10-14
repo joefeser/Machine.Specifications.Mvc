5b0a2646c0a7763f7ffc70a8b253f810bf45d055
Nathan Roe <roend83@gmail.com>
2012-10-14 14:17:45 -0400

    Refactored "And" objects to a single generic object
    Changed ViewResultExtensions to ViewResultBaseExtensions

---
+++ Source/Machine.Specifications.Mvc/ActionResultAnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace Machine.Specifications.Mvc
+{
+    public class ActionResultAnd<TActionResult>
+        where TActionResult : ActionResult
+    {
+        readonly TActionResult actionResult;
+
+        public ActionResultAnd(TActionResult actionResult)
+        {
+            this.actionResult = actionResult;
+        }
+
+        public TActionResult And()
+        {
+            return actionResult;
+        }
+    }
+}
---
+++ Source/Machine.Specifications.Mvc/ViewResultBaseExtensions.cs
@@ -0,0 +1,34 @@
+namespace Machine.Specifications.Mvc
+{
+    using System.Web.Mvc;
+
+    public static class ViewResultBaseExtensions
+    {
+        public static ActionResultAnd<TViewResult> ShouldUseView<TViewResult>(this TViewResult viewResult, string viewName)
+            where TViewResult : ViewResultBase
+        {
+            viewResult.ViewName.ShouldEqual(viewName);
+            return new ActionResultAnd<TViewResult>(viewResult);
+        }
+
+        public static ActionResultAnd<TViewResult> ShouldUseDefaultView<TViewResult>(this TViewResult viewResult)
+            where TViewResult : ViewResultBase
+        {
+            viewResult.ViewName.ShouldBeEmpty();
+            return new ActionResultAnd<TViewResult>(viewResult);
+        }
+
+        public static ModelTypeAnd<T> ShouldHaveModelOfType<T>(this ViewResultBase viewResult)
+        {
+            viewResult.ViewData.Model.ShouldBeOfType<T>();
+            return new ModelTypeAnd<T>((T) viewResult.ViewData.Model);
+        }
+
+        public static ActionResultAnd<TViewResult> ShouldNotHaveAModel<TViewResult>(this TViewResult viewResult)
+            where TViewResult : ViewResultBase
+        {
+            viewResult.Model.ShouldBeNull();
+            return new ActionResultAnd<TViewResult>(viewResult);
+        }
+    }
+}
